aaab8bd77ce7f296f91c97aee8d333567b50e43a
Tooyaes <23fi005@ms.dendai.ac.jp>
2025-06-13 12:45:27 +0900

    addIndex

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse
+
+
 import random  # randomライブラリを追加
 
 app = FastAPI()
@@ -33,3 +36,17 @@ def omikuji():
     ]
 
     return omikuji_list[random.randrange(10)]
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
